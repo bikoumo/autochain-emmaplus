48b8518a3ceedb14d897ecf15d87f1b44eae11b1
Theresa <bikoumoutheresa@gmail.com>
2026-07-22 12:28:30 +0100

    Ajout de trustProxies pour corriger l erreur 419 sur Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +10,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Faire confiance aux proxys (nécessaire pour Render et le HTTPS)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
